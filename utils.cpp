788dbaef25633b146aadfc2bc7c44f26f0ee98c4
ostolopov <egor21231bar@gmail.com>
2025-09-16 21:19:18 +0300

    ignore

---
+++ utils.cpp
@@ -0,0 +1,23 @@
+#include "utils.h"
+
+int getInt(int &number, int min, int max) {
+    while (true) {
+        if (!(std::cin >> number)) {
+            if (std::cin.eof()) {
+                std::cout << "\nProgram terminated.\n";
+                return 0;
+            }
+            std::cout << "Invalid number. Please try again!\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            continue;
+        }
+        if (number < min || number > max) {
+            std::cout << "The number is out of the allowed range. Please try again!\n";
+            continue;
+        }
+        break;
+    }
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return 1;
+}
---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <iostream>
+
+int getInt(int &number, int min, int max);
+
+#endif
